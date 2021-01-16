fe3d8e822b6ac255e1712639d7382622275cb344
tfbninja <tfbninja@users.noreply.github.com>
2021-01-16 16:27:21 -0700

    it lowkey works now

---
+++ NowPlaying.py
@@ -0,0 +1,50 @@
+import time
+class NowPlaying:
+    def __init__(self, sp):
+        self.sp = sp
+        self.syncSongData()
+
+    def syncSongData(self):
+        self.results = self.sp.current_user_playing_track()
+        self.progress_ms = self.results['progress_ms']
+        self.msStart = time.time()
+        
+        self.name = self.results['item']['name']
+        self.uri = self.results['item']['uri']
+        self.songLength = self.results['item']['duration_ms']
+        self.isPlaying = self.results['is_playing']
+        
+        self.analysis = self.sp.audio_analysis(self.uri)
+        self.beatlist = self.analysis['beats']
+        
+        self.features = self.sp.audio_features(self.uri)
+        self.tempo = self.features[0]['tempo']
+
+    def reSync(self):
+        tempResults = self.sp.current_user_playing_track()
+        if self.uri != tempResults['item']['uri']:
+            self.syncSongData()
+            return()
+        self.progress_ms = tempResults['progress_ms']
+        self.msStart = time.time()
+        self.isPlaying = tempResults['is_playing']
+        
+    def getBeatlist(self):
+        return self.beatlist
+
+    def getPosInSongMillis(self):
+        deltaMS = (time.time() - self.msStart) * 1000
+        return deltaMS + self.progress_ms
+
+    def getSongName(self):
+        return self.name
+
+    def getTempo(self):
+        return self.tempo
+
+    def getSongLengthMillis(self):
+        return self.songLength
+
+    def getIsPlaying(self):
+        return self.isPlaying
+
--- SpotifyLiaison.py
+++ SpotifyLiaison.py
@@ -1,36 +1,106 @@
-import spotipy
-import spotipy.util as util
 from credentials import *
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
-token = util.prompt_for_user_token("tfbninja", "nonescope", client_id=id,
-                                   client_secret=secret, redirect_uri='your-app-redirect-url')
-
-
-def show_tracks(results):
-    for i, item in enumerate(results['items']):
-        track = item['track']
-        print(
-            "   %d %32.32s %s" %
-            (i, track['artists'][0]['name'], track['name']))
+import time
+import sys
+from math import *
+import serial
+import json
+from colour import Color
+import NowPlaying
 
 
-scope = 'playlist-read-private'
-sp = spotipy.Spotify(auth=token)
+def getTimeToNextBeat(millis, beatlist):
+    time = millis / 1000
+    for beatIndice in range(len(beatlist) - 1, 0, -1):
+        if time > beatlist[beatIndice]['start']:
+            return beatlist[beatIndice + 1]['start'] - time
+    return None
+def getTimeSinceBeat(millis, beatlist):
+    time = millis / 1000
+    for beatIndice in range(len(beatlist) - 1, 0, -1):
+        if time > beatlist[beatIndice]['start']:
+            return time - beatlist[beatIndice - 1]['start']
+    return None
+def printDelta(start, message):
+    print(str(time.time() - start) + "   " + message)
+    
 
-playlists = sp.current_user_playlists()
-user_id = sp.me()['id']
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=id,
+                                               client_secret=secret,
+                                               redirect_uri="https://example.com/callback",
+                                               scope="user-read-currently-playing"))
 
-for playlist in playlists['items']:
-    if playlist['owner']['id'] == user_id:
-        print()
-        print(playlist['name'])
-        print('  total tracks', playlist['tracks']['total'])
+results = sp.current_user_playing_track()
 
-        results = sp.playlist(playlist['id'], fields="tracks,next")
-        tracks = results['tracks']
-        show_tracks(tracks)
+progress_ms = results['progress_ms']
+msStart = time.time()
+print("progress in ms: " + str(progress_ms))
 
-        while tracks['next']:
-            tracks = sp.next(tracks)
-            show_tracks(tracks)
+uri = results['item']['uri']
+print("uri: " + str(results["item"]["uri"]))
+
+"""
+for i in results:
+    print(str(i) + ": " + str(results[i]))
+"""
+
+print("is playing: " + str(results['is_playing']) + "\n\n\n")
+
+start = time.time()
+analysis = sp.audio_analysis(uri)
+delta = time.time() - start
+
+print("analysis retrieved in %.2f seconds" % (delta,))
+"""
+for i in analysis:
+    print(str(i) + str(analysis[i]))
+"""
+
+currentSong = NowPlaying.NowPlaying(sp)
+delta = time.time() - msStart
+last = 0
+
+ser = serial.Serial('COM3', 74880)
+
+c = Color("blue")
+
+loopIndice = 0
+while(True):
+    loopIndice += 1
+    #delta = time.time() - msStart
+    #currentMS = progress_ms + delta * 1000
+
+    if c.hue < 0.998:
+        c.hue = c.hue + 0.001
+    else:
+        c.hue = 0
+
+    successful = False
+    while not successful:
+        try:
+            nextBeat = getTimeToNextBeat(currentSong.getPosInSongMillis(), currentSong.getBeatlist())
+            successful = True
+        except IndexError:
+            currentSong.reSync()
+
+    lastBeat = getTimeSinceBeat(currentSong.getPosInSongMillis(), currentSong.getBeatlist())
+
+    ratio =  (nextBeat ** 2) / (nextBeat + lastBeat ** 2)
+
+    c.luminance = ratio + 0.05
+
+    out = '(' + str(floor(c.red * 255)) + ',' + str(floor(c.green * 255)) + ',' + str(floor(c.blue * 255)) + ')'
+
+    print("name: " + currentSong.getSongName() + "  is playing: " + str(currentSong.getIsPlaying()) + "   " + out + "   " + str(loopIndice))
+
+    ser.write(bytes(out, 'utf-8'))
+
+    if loopIndice % 150 == 0:
+        ser.flushInput()
+        ser.flushOutput()
+    if (loopIndice % 2000 == 0):
+        currentSong.reSync()
+        loopIndice = 0
+    
+    #time.sleep(0.001)    
